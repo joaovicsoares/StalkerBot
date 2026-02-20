0b3f0a9895e8b09902632f8878c14e123ee137b7
joaovicsoares <victorjoaosoa@gmail.com>
2026-02-19 23:06:04 -0300

    feat: chamando funcoes no main.py

--- main.py
+++ main.py
@@ -15,9 +15,15 @@ def main():
     for seguidor in seguidores:
         print (seguidor["username"])
 
+    seguidoresAntigos = processaDados.recuperaXls(Config.PROFILE)
+
+    seguidoresDiferentes = processaDados.comparaSeguidores(seguidoresAntigos, seguidores)
+
     processaDados.salvaXls(seguidores, Config.PROFILE)
 
     print(f"\nTotal capturado via API: {len(seguidores)}")
+    print(f"\nnovos seguidores:{seguidoresDiferentes['novos_seguidores']}")
+    print(f"\nparou de seguir:{seguidoresDiferentes['deixaram_de_seguir']}")
 
     input("Pressione ENTER para sair...")
 
